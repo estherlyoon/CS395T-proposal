0c494398711ae87f88fb41a13fe08b9d23f25133
estherlyoon <estherlyoon@gmail.com>
2023-11-30 23:35:47 -0700

    Scale deployments from controller

--- controller/controller.py
+++ controller/controller.py
@@ -1,9 +1,31 @@
+from __future__ import print_function
+from kubernetes import config, client
+from kubernetes.client.rest import ApiException
+import time
 from typing import Tuple
 
 pod_types = {}
 pod_configs = {}
 service_queues = {}
 pod_queues = {}
+
+def scale_deployment(name, scale):
+    api_instance = client.AppsV1Api()
+
+    # Get current number of replicas
+    curr_replicas = 1
+    try:
+        read_resp = api_instance.read_namespaced_deployment_scale(name, 'default')
+        curr_replicas = read_resp['spec']['replicas'] # TODO haven't checked, but smth like this
+    except ApiException as e:
+        print("Exception when calling AppsV1Api->read_namespaced_deployment_scale: %s\n" % e)
+
+    body = {'spec': {'replicas': curr_replicas+scale}}
+    try:                                                                        
+        scale_resp = api_instance.patch_namespaced_deployment_scale(name, 'default', body)
+        print(scale_resp)
+    except ApiException as e:
+        print("Exception when calling AppsV1Api->patch_namespaced_deployment_scale: %s\n" % e)
 
 def get_info_from_prometheus():
     # TODO: some kind of http request to get status info
@@ -24,27 +46,30 @@ def scale_replicas(num: int, deployment_name: str):
     pass
 
 def main():
-    # TODO: get status from prometheus
-    status = get_info_from_prometheus()
+    config.load_incluster_config()
+    scale_interval = 10
 
-    if pod_id not in pod_types:
-        # TODO: get pod info from k8s
-        pod_types[pod_id] = service_name
-        pod_configs[pod_id] = "get me from k8s"
-    
-    if service_name not in service_queues:
-        service_queues[service_name] = queue_length
-    else:
-        service_queues[service_name] = service_queues[service_name] - pod_queues[pod_id] + queue_length
+    while True:
+        # TODO: get status from prometheus
+        status = get_info_from_prometheus()
 
-    pod_queues[pod_id] = queue_length
-    if scale_up(service_queues[service_name]):
-        # TODO: add a new pod of that service
-        pass
-    elif scale_down(service_queues[service_name]):
-        # TODO: remove a pod of that service
-        pass
+        if pod_id not in pod_types:
+            # TODO: get pod info from k8s
+            pod_types[pod_id] = service_name
+            pod_configs[pod_id] = "get me from k8s"
+        
+        if service_name not in service_queues:
+            service_queues[service_name] = queue_length
+        else:
+            service_queues[service_name] = service_queues[service_name] - pod_queues[pod_id] + queue_length
 
+        pod_queues[pod_id] = queue_length
+        if scale_up(service_queues[service_name]):
+            scale_deployment(service_name, 1)
+        elif scale_down(service_queues[service_name]):
+            scale_deployment(service_name, -1)
+
+        time.sleep(scale_interval)
 
 if __name__ == "__main__":
     main()
